3668918a962dc1b7f8b3c165a12befe0fabf4dad
Яков Клейменов <yakovkleimenov@yandex.ru>
2022-12-02 19:24:35 +0400

    Last changes

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ def attack(char_name: str, char_class: str) -> str:
     if char_class == 'healer':
         return (f'{char_name} нанёс урон противнику равный '
                 '{5 + randint(-3, -1)}')
+    return (f'{char_name} не нанёс урон противнику')
 
 
 def defence(char_name: str, char_class: str) -> str:
@@ -24,6 +25,7 @@ def defence(char_name: str, char_class: str) -> str:
         return (f'{char_name} блокировал {10 + randint(-2, 2)} урона')
     if char_class == 'healer':
         return (f'{char_name} блокировал {10 + randint(2, 5)} урона')
+    return (f'{char_name} не не заблокировал урон')
 
 
 def special(char_name: str, char_class: str) -> str:
@@ -38,6 +40,7 @@ def special(char_name: str, char_class: str) -> str:
         return (f'{char_name} применил специальное умение «Атака {5 + 40}»')
     if char_class == 'healer':
         return (f'{char_name} применил специальное умение «Защита {10 + 30}»')
+    return (f'{char_name} не применил специальное умение')
 
 
 def start_training(char_name: str, char_class: str) -> str:
